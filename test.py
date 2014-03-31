b42800a126a1e4eaf11cd4feaa210ae65671314f
htyuan <htyuan80@gmail.com>
2014-04-01 00:07:29 +0800

    我定义正确了一个类和一个方法。以后主要精力放在测试驱动开发上

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 class WasRun:
 	def __init__(self, name):
 		self.wasRun=None
+	
+	def testMethod(self):
+		self.wasRun=True
+
 if __name__ == "__main__":
 	test = WasRun("testMethod")
 	print test.wasRun
-	#test.testMethod()
-	#print test.wasRun
+	test.testMethod()
+	print test.wasRun
 
 
